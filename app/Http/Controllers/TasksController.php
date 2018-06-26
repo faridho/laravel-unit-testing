653b5a490129c9ab7fc6ab47888973bd93ce9857
faridho <=>
2018-06-26 16:23:01 +0700

    First Commit

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+class TasksController extends Controller
+{
+    public function index(){
+        $tasks = Task::all();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function store(){
+        Task::create(request()->only('name', 'description'));
+        return back();
+    }
+}
---
+++ tests/Feature/ManageTasksTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ManageTasksTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function user_can_create_task(){
+        //this is default --> $this->assertTrue(true);
+
+        //user buka halaman daftar task
+        $this->visit('/tasks');
+
+        //isi form name dan description kemudian submit
+        $this->submitForm('Create Task', [
+            'name'          => 'My First Task',
+            'description'   => 'This is my first task on my job'
+        ]);
+
+        //lihat record tersimpan ke database
+        $this->seeInDatabase('tasks', [
+            'name'          => 'My First Task',
+            'description'   => 'This is my first task on my job',
+            'is_done'       => 0
+        ]);
+
+        //redirect ke halaman daftar task
+        $this->seePageIs('/tasks');
+
+        //tampil hasil task yang telah diinput
+        $this->see('My First Task');
+        $this->see('This is my first task on my job');
+    }
+
+    /** @test */
+    public function user_can_browser_tasks_index_page(){
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function user_can_edit_an_existing_task(){
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function user_can_delete_an_existing_task(){
+        $this->assertTrue(true);
+    }
+}
